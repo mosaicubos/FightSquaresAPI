5e9ef16d8cf8b7128511014c5c38f4a18f480df7
TuPapasitoSexy <148282461+TuPapasitoSexy@users.noreply.github.com>
2025-01-03 02:11:31 +0100

    Prueba Backend con una palabra
    Para probar si se envia y se recibe una palabra correctamente

--- squareFightsAPI.js
+++ squareFightsAPI.js
@@ -1,1 +1,23 @@
-console.log("hola q tal")
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+
+let currentWord = "";
+
+
+app.post("/send", (req,res) =>{
+    const {word} = req.body;
+    if(!word) return res.status(400).json({error: "No se ha recibido una palabra"});
+    currentWord = word;
+    return res.status(200).json({message: "Palabra recibida"})
+});
+
+
+app.get("/receive", (req,res) =>{
+    if(!currentWord) return res.status(400).json({error: "No existe la palabra"});
+    return res.status(200).json({word:currentWord})
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
