da02dd7f13be96f8dae6093d44d7424f39ca072e
TuPapasitoSexy <148282461+TuPapasitoSexy@users.noreply.github.com>
2025-01-03 03:15:04 +0100

    Made backend simple for less trafic

--- squareFightsAPI.js
+++ squareFightsAPI.js
@@ -7,16 +7,16 @@ let currentWord = "";
 
 
 app.post("/send", (req,res) =>{
-    const {word} = req.body;
-    if(!word) return res.status(400).json({error: "No se ha recibido una palabra"});
+    const word = req.body;
+    if(!word) return res.status(404).send("No hay palabra");
     currentWord = word;
-    return res.status(200).json({message: "Palabra recibida"})
+    return res.status(200).send("Palabra recibida")
 });
 
 
 app.get("/receive", (req,res) =>{
-    if(!currentWord) return res.status(400).json({error: "No existe la palabra"});
-    return res.status(200).json({word:currentWord})
+    if(!currentWord) return res.status(404).send("No word available")
+    return res.status(200).send(currentWord)
 })
 
 const PORT = process.env.PORT || 3000;
